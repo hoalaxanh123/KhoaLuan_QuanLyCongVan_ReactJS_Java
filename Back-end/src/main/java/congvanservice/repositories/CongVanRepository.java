6644ffa8548b59c49ed946dc647d49b6bbfa6950
Vuong Nguyen <nguyenvanvuong972@gmail.com>
2019-12-08 09:45:48 +0700

    remove unused folder

--- Back-end/src/main/java/congvanservice/repositories/CongVanRepository.java
+++
@@ -1,9 +0,0 @@
-package congvanservice.repositories;
-
-import congvanservice.models.CongVan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CongVanRepository extends JpaRepository<CongVan, Integer> {
-}
